32711aa21c2bdd1c72cb68e61af9b4999d882f56
František Holubec <frantisek.holubec@gmail.com>
2025-09-30 20:57:29 +0200

    warning meta for nonexisting folder

--- plugin-unity-agent/src/main/kotlin/jetbrains/buildServer/unity/logging/LineStatusProvider.kt
+++ plugin-unity-agent/src/main/kotlin/jetbrains/buildServer/unity/logging/LineStatusProvider.kt
@@ -38,6 +38,8 @@ class LineStatusProvider {
                 Regex("Identifier uniqueness violation:.*") to LineStatus.Warning,
                 Regex("Missing types referenced from.*?") to LineStatus.NonFatalFailure,
                 Regex("OVRPlugin not updated. Restart the editor to update.*") to LineStatus.NonFatalFailure,
+                Regex("A meta data file \\(\\.meta\\) exists but its folder .* can't be found.*") to LineStatus.Warning,
+
         ))
     }
 
