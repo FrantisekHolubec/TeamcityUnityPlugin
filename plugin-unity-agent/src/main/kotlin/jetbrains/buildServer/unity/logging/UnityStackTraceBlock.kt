bf2b38ee61cc8bdcc2e0c48b20c3d09321f4054a
František Holubec <frantisek.holubec@gmail.com>
2025-10-02 08:13:58 +0200

    Unity stack trace block filter out useless stuff

--- plugin-unity-agent/src/main/kotlin/jetbrains/buildServer/unity/logging/UnityStackTraceBlock.kt
+++ plugin-unity-agent/src/main/kotlin/jetbrains/buildServer/unity/logging/UnityStackTraceBlock.kt
@@ -10,23 +10,31 @@ class UnityStackTraceBlock : LogBlock {
 
     override fun isBlockEnd(text: String) = blockEndPatterns.any { it.containsMatchIn(text) }
 
-    override fun getText(text: String) = text
+    override fun getText(text: String) =
+        if (filterOut.any { it.containsMatchIn(text) }) ""
+        else text
 
     companion object {
         private val blockStartPatterns = listOf(
             Regex("UnityEngine.*ExtractStackTrace"),
-            Regex("UnityEngine.StackTraceUtility:ExtractStackTrace.*"),
-            Regex("UnityEditor.BuildPipeline:BuildPlayerInternalNoCheck.*$")
+            Regex("UnityEngine\\.StackTraceUtility:ExtractStackTrace.*"),
+            Regex("UnityEditor\\.BuildPipeline:BuildPlayerInternalNoCheck.*$")
         )
 
         private val blockEndPatterns = listOf(
             Regex("(^\\s*\\(Filename:.*)|(\\s*\\[.* line \\d+\\])"),
-            Regex("UnityEditor.Scripting.ScriptCompilation.EditorCompilationInterface:(TickCompilationPipeline|CompileScripts).*"),
-            Regex("UnityEditorInternal.APIUpdating.APIUpdaterManager:ProcessImportedAssemblies.*"),
-            Regex("UnityEditor.Modules.ModuleManager:InitializePlatformSupportModules.*"),
-            Regex("UnityEditor.EditorApplication:Internal_CallDelayFunctions.*"),
-            Regex("UnityEditor.EditorAssemblies:ProcessInitializeOnLoadMethodAttributes.*"),
-            Regex("UnityEditor.AssetPostprocessingInternal:PostprocessAllAssets.*")
+            Regex("UnityEditor\\.Scripting\\.ScriptCompilation\\.EditorCompilationInterface:(TickCompilationPipeline|CompileScripts).*"),
+            Regex("UnityEditorInternal\\.APIUpdating\\.APIUpdaterManager:ProcessImportedAssemblies.*"),
+            Regex("UnityEditor\\.Modules\\.ModuleManager:InitializePlatformSupportModules.*"),
+            Regex("UnityEditor\\.EditorApplication:Internal_CallDelayFunctions.*"),
+            Regex("UnityEditor\\.EditorAssemblies:ProcessInitializeOnLoadMethodAttributes.*"),
+            Regex("UnityEditor\\.AssetPostprocessingInternal:PostprocessAllAssets.*")
+        )
+
+        private val filterOut = listOf(
+            Regex("^UnityEngine\\.Debug:.*$"),
+            Regex("^UnityEngine\\.Logger:.*$"),
+            Regex("^UnityEngine\\.StackTraceUtility:.*$")
         )
     }
 }
